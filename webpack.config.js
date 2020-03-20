c6a1e8fc2742378c2066b020858c7b826b5305e2
oti <otiext@gmail.com>
2020-03-20 22:08:36 +0900

    TypeScriptをwebpackでバンドルするための設定ファイルを追加した

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  mode: 'production',
+  entry: {
+    script: './src/script/script.ts'
+  },
+  output: {
+    filename: '[name].js',
+    path: __dirname + '/dist/script'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts']
+  }
+};
