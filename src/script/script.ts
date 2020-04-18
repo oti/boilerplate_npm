326874a26c3bfd605ff2755956c93868a824c8e2
oti <otiext@gmail.com>
2020-04-18 18:46:10 +0900

    core-js と regenerator-runtime でIE11でも動くようにした

--- src/script/script.ts
+++ src/script/script.ts
@@ -1,6 +1,14 @@
+// for IE11
+import "core-js/stable";
+import "regenerator-runtime/runtime";
+
 import { InView } from "./module/InView";
 
 document.addEventListener("DOMContentLoaded", (event) => {
   const inView = new InView();
   inView.init();
+
+  (document.querySelectorAll('*') as NodeListOf<HTMLElement>).forEach((elem, i) => {
+    console.log(elem)
+  })
 });
