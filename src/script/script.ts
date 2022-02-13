b66e7d912937160c7b50662a3223da4e5739ef3c
oti <otiext@gmail.com>
2022-02-13 16:26:50 +0900

    fix(script): remove `viewportFit`

--- src/script/script.ts
+++ src/script/script.ts
@@ -1,10 +1,6 @@
 import { InView } from "./module/InView";
-import { ViewportFit } from "./module/ViewportFit";
 
 document.addEventListener("DOMContentLoaded", (event) => {
   const inView = new InView();
   inView.init();
-
-  const viewportFit = new ViewportFit();
-  viewportFit.init();
 });
